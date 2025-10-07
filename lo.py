4cfd3054f4b695577cb98af80e53d871399c23e5
Mohira Abdullayeva <abdullaevamoxira572@gmail.com>
2025-10-07 10:11:18 +0300

    Initial comit:Creat

---
+++ lo.py
@@ -0,0 +1,24 @@
+user_name=input("please enter your name: ")
+home_planet=input("enter your home planet: ")
+species=input("your species: ")
+rank=input("enter your official rank: ")
+id_number=input("enter your ID number: ")
+
+
+id_code=("GF-"+ id_number+"-"+ home_planet)
+
+print("=================================")
+print("    GALACTIC FEDERATION ID CARD")
+print("=================================")
+print()
+print('Full name;    '+user_name)
+print( "HOME Planet; "+home_planet)
+print("Species:"+species)
+print("Rank:"+rank)
+
+
+print()
+print("----------------------------------")
+print("ID Code: "+ id_code)
+      
+print("=====================================")
